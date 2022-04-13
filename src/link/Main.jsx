2aeea75809c56dd74b1ca5f8029428cb7bf8008c
MiBBy-4 <maksim.762373maks@bk.ru>
2022-04-13 19:51:49 +0300

    feat: realize ability to short link only for logged in users

--- src/link/Main.jsx
+++ src/link/Main.jsx
@@ -2,12 +2,14 @@ import {
   Grid,
   Button,
   Typography,
-  Textarea,
+  Alert,
+  AlertTitle,
 } from '@mui/material';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import { Form } from 'react-bootstrap';
 import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { postLink } from '../apiRequests/LinkRequests';
 
@@ -17,6 +19,8 @@ export default function Main() {
     description: '',
   });
 
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((status) => status.isLoggedIn);
   const navigate = useNavigate();
 
   function handleIdeaChange(event) {
@@ -64,14 +68,23 @@ export default function Main() {
               {' '}
               <a href="/links">table</a>
             </Typography>
+            { isLoggedIn ? (null) : (
+              <Alert severity="warning" className="my-3">
+                <AlertTitle>Not logged in!</AlertTitle>
+                To short your link you should
+                <strong> log in</strong>
+              </Alert>
+            ) }
             <form onSubmit={handleSubmit}>
               <TextField fullWidth label="Insert link" id="base_link" name="base_link" onChange={handleIdeaChange} />
               <Form.Group className="mt-1">
                 <Form.Control as="textarea" rows={3} placeholder="Description of Link" name="description" onChange={handleIdeaChange} />
               </Form.Group>
-              <Button fullWidth variant="outlined" color="success" size="large" className="mt-1" type="submit">
-                Short It!
-              </Button>
+              { isLoggedIn ? (
+                <Button fullWidth variant="outlined" color="success" size="large" className="mt-1" type="submit">
+                  Short It!
+                </Button>
+              ) : (null) }
             </form>
           </Box>
         </Grid>
