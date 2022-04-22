ddc0aabe5c136fdf0bb83f4a015132339cde31a3
MiBBy-4 <maksim.762373maks@bk.ru>
2022-04-22 08:50:15 +0300

    refactor: change all react-bootstrap elements on mui elements in LinkShow component

--- src/views/link/LinkShow.jsx
+++ src/views/link/LinkShow.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router';
 import {
-  Container,
+  Box,
   Card,
-  ListGroup,
-  ListGroupItem,
-} from 'react-bootstrap';
+  CardActions,
+  CardContent,
+  Button,
+  Typography,
+} from '@mui/material';
+import { useParams } from 'react-router';
 import { getLink } from '../../requests/apiRequests/LinkRequests';
 
 export default function LinkShow(props) {
@@ -25,36 +27,20 @@ export default function LinkShow(props) {
   });
 
   return (
-    <Container>
-      <Card className="mt-5 py-2 px-2" border="secondary">
-        <Card.Title className="text-center">
-          { link.base_link }
-        </Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">
+    <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          <b>Base link: </b>
+          <a href={link.base_link}>{link.base_link}</a>
+        </Typography>
+        <Typography variant="h5" component="div">
+          <b>Shorted link: </b>
+          <a href={`${process.env.REACT_APP_API_URL}${link.shorted_link}`}>{link.shorted_link}</a>
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
           { link.description }
-          <div className="d-flex justify-content-center">
-            <div className="tags-input-container w-25 mt-2 d-flex justify-content-center">
-              { tags.map((tag, index) => (
-                <div className="tags-item" key={index}>
-                  <span className="text">{tag.tag_name}</span>
-                </div>
-              ))}
-            </div>
-          </div>
-        </Card.Subtitle>
-        <Card.Body>
-          <ListGroup className="list-group=flush">
-            <ListGroupItem>
-              <b>Base link: </b>
-              <a href={link.base_link}>{link.base_link}</a>
-            </ListGroupItem>
-            <ListGroupItem>
-              <b>Shorted link: </b>
-              <a href={`${process.env.REACT_APP_API_URL}${link.shorted_link}`}>{link.shorted_link}</a>
-            </ListGroupItem>
-          </ListGroup>
-        </Card.Body>
-        <Card.Footer className="text-center">
+        </Typography>
+        <Typography variant="body2">
           <b>Created At:</b>
           {` ${link.created_at} `}
           {link.user_id === user.id &&
@@ -62,8 +48,17 @@ export default function LinkShow(props) {
               <b>views: </b>
               {` ${link.views} `}
             </div> }
-        </Card.Footer>
-      </Card>
-    </Container>
+        </Typography>
+      </CardContent>
+      <div className="d-flex justify-content-center">
+        <div className="tags-input-container w-25 mt-2 d-flex justify-content-center">
+          { tags.map((tag, index) => (
+            <div className="tags-item" key={index}>
+              <span className="text">{tag.tag_name}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Card>
   );
 }
